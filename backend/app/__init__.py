bc06426716dd947c156e8c617512408e8b992163
samarthtambad <tambad.samarth@gmail.com>
2020-07-16 19:48:15 -0400

    add __init__.py to make app a package

---
+++ backend/app/__init__.py
@@ -0,0 +1,32 @@
+# contains application factory and tells Python that the app directory should be treated as a package
+
+import os
+from flask import Flask
+
+
+def create_app(test_config=None):
+    # create and configure the app
+    app = Flask(__name__, instance_relative_config=True)
+
+    app.config.from_mapping(
+        SECRET_KEY='dev'    # TODO: change this in production
+    )
+
+    if test_config is None:
+        # load the instance config, if it exists, when not testing
+        app.config.from_pyfile('config.py', silent=True)
+    else:
+        # load the test config if passed in
+        app.config.from_mapping(test_config)
+
+    try:
+        os.makedirs(app.instance_path)
+    except OSError as error:
+        print(error)
+
+    # a simple page that says hello
+    @app.route('/test')
+    def test():
+        return 'Hello there! Your app is working.'
+
+    return app
