05b757a9e93581b7c1bce49acee06d3a1feb2dec
samarthtambad <tambad.samarth@gmail.com>
2020-07-27 22:48:59 -0400

    add push to firestore

--- backend/app/create.py
+++ backend/app/create.py
@@ -1,19 +1,31 @@
 
 import uuid
 from flask import Blueprint
+from flask import request
 from firebase_admin import firestore
 
 bp = Blueprint('create', __name__, url_prefix='/api')
 db = firestore.client()
 
-@bp.route('/create', methods=(['GET', 'POST']))
+@bp.route('/create', methods=(['POST']))
 def create():
-	print("create")
+	print("/api/create", request.method)
+	req_obj = request.get_json()
+	# print(req_obj)
+	
 	id = str(uuid.uuid4())
+	
+	# create document to store
+	doc = {}
+	doc["id"] = id
+	doc["meetingName"] = req_obj['data']['meetingName']
+	doc["creatorName"] = "Creator"		# TODO: send meeting creator name from front-end
+	doc["dates"] = req_obj['data']['dates']
+	# print(doc)
+
+	# push to firestore
 	doc_ref = db.collection(u'meetings').document(id)
-	doc_ref.set({
-    	u'title': u'Birthday Party',
-    	u'participants': u'participants list'
-	})
+	doc_ref.set(doc)
+	
 	return id
 
