d24f936f1bd9b9ab4cc8326a1fac6cb501303ba8
samarthtambad <tambad.samarth@gmail.com>
2020-07-27 22:44:05 -0400

    change /create to post request

--- frontend/src/utils/http_functions.js
+++ frontend/src/utils/http_functions.js
@@ -4,7 +4,7 @@ import axios from 'axios';
 export function submit_meeting(data) {
 	// console.log("submit_meeting");
 	// console.log(data);
-	return axios.get('/api/create', {
+	return axios.post('/api/create', {
 		data
 	});
 }
