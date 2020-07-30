234420a836d1eaaf6c581d0809fbae7e67d4f376
Katherine Pully <kpully@gmail.com>
2020-07-29 21:00:31 -0600

    coordinate.js display all datse from start to end date inclusive

--- frontend/src/components/Coordinate.js
+++ frontend/src/components/Coordinate.js
@@ -1,13 +1,25 @@
-import React, {Component, useState} from 'react';
+import React, {Component} from 'react';
 import { get_meeting_data } from '../utils/http_functions';
 import { Container, Row, Col } from 'react-bootstrap';
+import {Table} from './Table';
+
 
 export class Coordinate extends React.Component {
 	constructor(props) {
 		super(props);
 		this.state = {
 			meetingId: this.props.match.params.meetingId,
-			meetingName:""
+			meetingName:"",
+			meetingDates:["x"],
+			tableData:[
+	          {'fruit': 'Apple', 'cost': 100},
+	          {'fruit': 'Orange', 'cost': 50},
+	          {'fruit': 'Banana', 'cost': 35},
+	          {'fruit': 'Mango', 'cost': 70},
+	          {'fruit': 'Pineapple', 'cost': 45},
+	          {'fruit': 'Papaya', 'cost': 40},
+	          {'fruit': 'Watermelon', 'cost': 35}
+	        ]
 		}
 	};
 
@@ -16,15 +28,21 @@ export class Coordinate extends React.Component {
 	 	.then(response => {
  			console.log(response);
  			this.setState({
- 				meetingName: response["data"]["meetingName"]
+ 				meetingName: response["data"]["meetingName"],
+ 				meetingDates: response["data"]["dates"]
  			});
 		})
   }
 
   render() {
-  	console.log(this.state.meetingName);
+  	console.log("coordinate");
+  	console.log(this.state.meeteingName);
+  	console.log(this.state.meetingDates);
   	return(
-  		<h1>{this.state.meetingName}</h1>
+  		<Container>
+	  		<h1>{this.state.meetingName}</h1>
+	  		<Table data={this.state.meetingDates}/>
+	  	</Container>
   	);
   
   }
---
+++ frontend/src/components/Table.js
@@ -0,0 +1,57 @@
+import React, {Component} from 'react';
+const Moment = require('moment');
+const MomentRange = require('moment-range');
+const moment = MomentRange.extendMoment(Moment);
+
+export class Table extends React.Component {
+	constructor(props) {
+		super(props);
+		console.log("table");
+		console.log(this.props.data);
+		// this.getHeader = this.getHeader.bind(this);
+		// this.getRowsData = this.getRowsData.bind(this);
+		// this.getKeys = this.getKeys.bind(this);
+	}
+
+	getDates(startDate, stopDate) {
+	    var dateArray = [];
+	    var currentDate = moment(startDate);
+	    var stopDate = moment(stopDate);
+	    while (currentDate <= stopDate+1) {
+	        dateArray.push( moment(currentDate).format('YYYY-MM-DD') )
+	        currentDate = moment(currentDate).add(1, 'days');
+	    }
+	    return dateArray;
+	}
+
+ 	getHeader() {
+		var keys = Object.keys(this.props.data);
+		const startDate=this.props.data[0];
+		const endDate=this.props.data[1];
+		console.log(startDate,endDate);
+		const dates=this.getDates(startDate,endDate);
+		console.log(dates);
+		// console.log("keys", this.props.data[0]);
+
+		return dates.map((date)=>{
+			// d=this.props.data[key]
+			return <th key={date}>{date.toUpperCase().slice(0,10)}</th>
+	 	})
+	 }
+
+
+
+	 render() {
+	 	return (
+	 		<div>
+	 			<table>
+	 				<thead>
+	 					<tr>{this.getHeader()}</tr>
+	 				</thead>
+	 				<tbody>
+	 				</tbody>
+	 			</table>
+	 		</div>
+	 	);
+	 }
+}
